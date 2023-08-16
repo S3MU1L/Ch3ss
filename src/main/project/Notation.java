31c493d530ae45868be864d0dc1d2e4b46d94f81
Samuel Malec <xmalec@fi.muni.cz>
2023-08-16 14:42:15 +0200

    add notation utility class

--- src/main/project/Notation.java
+++ src/main/project/Notation.java
@@ -1,6 +1,17 @@
-package src.main.project;/**
- * 
+package src.main.project;
+
+/**
  * @author Samuel Malec
  */
 public class Notation {
+    public static boolean isCorrectNotation(String notation) {
+        return true;
+    }
+
+    public static Coordinates coordsFromNotation(String notation) {
+        int x = Character.toLowerCase(notation.charAt(0)) - 'a';
+        int y = Board.BOARD_SIZE - 1 - Integer.parseInt(String.valueOf(notation.charAt(1)));
+        System.out.println(x + " " + y);
+        return new Coordinates(x, y);
+    }
 }
