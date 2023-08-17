99b6d71b9342d145ce9f80d52232078fe73c7125
Samuel Malec <xmalec@fi.muni.cz>
2023-08-17 13:20:42 +0200

    equals method in Coordinates

--- src/main/project/Coordinates.java
+++ src/main/project/Coordinates.java
@@ -1,7 +1,23 @@
 package src.main.project;
 
+import java.util.Objects;
+
 /**
  * @author Samuel Malec
  */
 public record Coordinates(int x, int y) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
 }
